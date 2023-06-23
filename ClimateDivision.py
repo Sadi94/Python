0da6dd90f5bb534625c618f103ba989b50b89f76
Sadi94 <sadimohammad65@gmail.com>
2023-06-23 20:47:13 +0600

    BMD Climate division

---
+++ ClimateDivision.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+train_df = pd.read_csv("D:\data.csv")
+
+
+# In[12]:
+
+
+train_df
+
+
+# In[10]:
+
+
+extracted_data.to_csv('new_file.csv',index = False)
+
+
+# In[1]:
+
+
+appended_df = existing_df.append(new_df, ignore_index=True)
+
+
+# In[ ]:
+
+
+
+
